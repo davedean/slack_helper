06156678a0de6ad974f0ed0d03e263336703dcb1
David Dean <dave.dean@gmail.com>
2023-10-14 09:07:00 +1100

    improvements

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/davedean/slack_helper
 
 go 1.21.2
+
+require (
+	github.com/ijt/go-anytime v1.9.2 // indirect
+	github.com/ijt/goparsify v0.0.0-20221203142333-3a5276334b8d // indirect
+)
--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/ijt/go-anytime"
 )
 
 type slackStatus struct {
@@ -24,8 +26,15 @@ func (s slackStatus) set(userToken string) {
 		Profile slackStatus `json:"profile"`
 	}
 
-	p := profile{Profile: s}
+	var p profile
+
+	/* if s.Text == "" && s.Emoji == "" {
+		p = profile{Profile: slackStatus{Text: "", Emoji: ""}}
+	} */
+	p = profile{Profile: s}
 	// end HAX
+
+	//fmt.Printf("%#v", p)
 
 	jsonBody, _ := json.Marshal(p)
 	bodyReader := bytes.NewReader(jsonBody)
@@ -50,35 +59,57 @@ func (s slackStatus) set(userToken string) {
 	}
 	//fmt.Println(res)
 
-	_, err = io.ReadAll(res.Body)
+	//_, err = io.ReadAll(res.Body)
+	responseBytes, err := io.ReadAll(res.Body)
 	if err != nil {
 		log.Fatalf("error reading HTTP response body: %v", err)
 	}
 
-	//	log.Println("We got the response:", string(responseBytes))
+	if res.StatusCode == 200 {
+		fmt.Println("ok")
+	} else {
+		log.Println("We got the response:", string(responseBytes))
+	}
 
 }
 
 func main() {
 
 	userToken := os.Getenv("USER_TOKEN")
+	if userToken == "" {
+		fmt.Println("Put your user token in your env as USER_TOKEN. https://api.slack.com/authentication/token-types#user")
+		return
+	}
+
+	if len(os.Args) != 4 {
+		fmt.Println("takes args, status_text, status_emoji, and optional status_expiry space seperated .. eg:")
+		fmt.Println("./main 'this is a text status' ':ant:' 'monday 9am' # set text, emoji until monday 9am")
+		fmt.Println("./main 'this is a text status' '' '' # set just a text status, immediately")
+		fmt.Println("./main '' '' '' # remove your current status, CURRENTLY BROKEN")
+		return
+	}
 
 	text := os.Args[1]
 	emoji := os.Args[2]
 
-	// parse time
-	expiryDate, err := time.Parse("2006-01-02", os.Args[3])
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
 	status := slackStatus{
-		Text:   text,
-		Emoji:  emoji,
-		Expiry: int(expiryDate.Unix()),
+		Text:  text,
+		Emoji: emoji,
 	}
+
+	// expiry is tricky ..
+	if os.Args[3] != "" {
+		expiryDate, err := anytime.Parse(os.Args[3], time.Now())
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		status.Expiry = int(expiryDate.Unix())
+
+	}
+	//fmt.Println(expiryDate)
 
 	status.set(userToken)
 
